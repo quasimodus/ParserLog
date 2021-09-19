d49b6e7fe48ec449d13a921d388187f1ecaee9a6
Igor <igor@eremichev-it.ru>
2021-09-20 00:45:19 +0300

    restore

--- simple1.py
+++ simple1.py
@@ -46,17 +46,17 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('Filename')],
+            [sg.Input(), sg.FileBrowse()],
+            [sg.OK(), sg.Cancel()]]
 
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Get filename example', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == 'Ok':
         break
     print('You entered ', write_csv(count(reader(values[0]))))
 
